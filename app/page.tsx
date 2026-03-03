f4f006c1b205019ac05c0429d854e6b09a99b1ff
willowlynn-peterson <willowlynn.peterson1@gmail.com>
2026-03-02 20:45:16 -0700

    Update page.tsx
    About section

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ import { BlogPosts } from 'app/components/posts'
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter"> {'Hi, Willow here.'}
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter"> {'Hi, Willow here. I'm an ex-PM, now designing digital experiences that feel natural and intentional.'}
       </h1>
       <p className="mb-4">
-        {`I'm an ex-PM, now designing digital experiences that feel natural and intentional.`}
+        {`I’m a Product Designer with 7 years of experience across customer experience and product management. I started at Delighted in Product Management to shape roadmaps and launch features before realizing I was most energized by designing the experience itself. I do my best work designing in small, fast-moving teams where there’s a direct line between an idea and seeing it in the hands of users.`}
       </p>
       <div className="my-8">
         <BlogPosts />
