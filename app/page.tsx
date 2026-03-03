8027e59a960899c65b04629ae88fa6a0dd072c89
willowlynn-peterson <willowlynn.peterson1@gmail.com>
2026-03-02 20:34:52 -0700

    Update page.tsx
    edits

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,10 @@ import { BlogPosts } from 'app/components/posts'
 export default function Page() {
   return (
     <section>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter"> {'Hi, Willow here.'}
+      </h1>
       <p className="mb-4">
-        {`Hi, Willow here.
-        I'm an ex-PM, now designing digital experiences that feel natural and intentional.`}
+        {`I'm an ex-PM, now designing digital experiences that feel natural and intentional.`}
       </p>
       <div className="my-8">
         <BlogPosts />
